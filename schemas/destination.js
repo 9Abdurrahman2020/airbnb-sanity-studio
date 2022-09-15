5cc0bd6c74e4d750894b40bec69ddf59bfc65ff3
Abdur Rahman <9abdurrahman2020@gmail.com>
2022-09-15 16:44:45 +0600

    first commit

---
+++ schemas/destination.js
@@ -0,0 +1,38 @@
+export default {
+  name: "destination",
+  type: "document",
+  title: "Destination",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+    },
+    {
+      name: "price",
+      type: "number",
+      title: "Price",
+    },
+    {
+      name: "date",
+      type: "string",
+      title: "Date",
+    },
+    {
+      name: "rating",
+      type: "number",
+      title: "Rating",
+    },
+
+    {
+      name: "kilometers",
+      type: "number",
+      title: "Kilometers",
+    },
+    {
+      name: "image",
+      type: "string",
+      title: "Image",
+    },
+  ],
+};
